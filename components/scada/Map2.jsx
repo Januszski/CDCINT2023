fdb0d40e872283248febd3664a0b96d1011b5032
Petrus Patronus <jan@Petruss-MacBook-Air.local>
2024-01-15 11:48:56 -0600

    more progress on scada

--- components/scada/Map2.jsx
+++ components/scada/Map2.jsx
@@ -17,16 +17,26 @@ const Map2 = () => {
     const map = new mapboxgl.Map({
       container: mapContainer.current,
       // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-      style: "mapbox://styles/mapbox/light-v11",
+      style: "mapbox://styles/iseman/clrch7ga5007k01pi8ax94dsj",
       zoom: 18,
-      center: [148.9819, -35.3981],
+      center: [-93.65096, 42.02842],
       pitch: 60,
       antialias: true, // create the gl context with MSAA antialiasing, so custom layers are antialiased
+      projection: "mercator",
     });
 
+    //41.5495300381441, -90.47326073955497
+    //41.673220857243486, -93.64322386917952
+    //41.595338887111275, -93.93928769088245
+    //41.63818689662921, -93.90323546313394
+    //41.7537767001945, -93.91914639147656
+    //41.47303538428101, -93.85214497557614
+    //41.43613965221123, -93.59330483795992
+    //41.78618713818969, -93.47997957942567
+    //41.632992292783015, -93.32384961062884
     // parameters to ensure the model is georeferenced correctly on the map
-    const modelOrigin = [148.9819, -35.39847];
-    const modelAltitude = 0;
+    const modelOrigin = [-93.32384961062884, 41.632992292783015];
+    const modelAltitude = 35;
     const modelRotate = [Math.PI / 2, 0, 0];
 
     const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
@@ -56,39 +66,25 @@ const Map2 = () => {
       type: "custom",
       renderingMode: "3d",
       onAdd: function (map, gl) {
-        console.log("PRE CAMERA");
-        console.log("THIS", this);
-        console.log("NEW CAM", new THREE.Camera());
-        // console.log(
-        //   "NEW CAM2",
-        //   new THREE.PerspectiveCamera(45, width / height, 1, 1000)
-        // );
         this.camera = new THREE.Camera();
-        console.log("HERE NOW ");
         this.scene = new THREE.Scene();
-        console.log("POST CAMERA");
 
         // create two three.js lights to illuminate the model
         const directionalLight = new THREE.DirectionalLight(0xffffff);
         directionalLight.position.set(0, -70, 100).normalize();
         this.scene.add(directionalLight);
-        console.log("POSTLIGHT");
 
         const directionalLight2 = new THREE.DirectionalLight(0xffffff);
         directionalLight2.position.set(0, 70, 100).normalize();
         this.scene.add(directionalLight2);
-        console.log("POSTLIGHT2");
-
+        //"https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf"
         // use the three.js GLTF loader to add the 3D model to the three.js scene
-        console.log("LOADER", new GLTFLoader());
         const loader = new GLTFLoader();
         console.log("POSTLOADER");
-        loader.load(
-          "https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf",
-          (gltf) => {
-            this.scene.add(gltf.scene);
-          }
-        );
+        loader.load("/mapass/substation/scene.gltf", (gltf) => {
+          this.scene.add(gltf.scene);
+        });
+
         this.map = map;
 
         // use the Mapbox GL JS map canvas for three.js
@@ -139,8 +135,380 @@ const Map2 = () => {
       },
     };
 
-    map.on("style.load", () => {
+    //41.5495300381441, -90.47326073955497
+    //41.54905396880391, -90.47484152468148
+    //41.66867079920033, -90.3903138939476
+    // parameters to ensure the model is georeferenced correctly on the map
+    const modelOrigin2 = [-90.3903138939476, 41.54905396880391];
+    const modelAltitude2 = 35;
+
+    const modelAsMercatorCoordinate2 = mapboxgl.MercatorCoordinate.fromLngLat(
+      modelOrigin2,
+      modelAltitude2
+    );
+
+    // transformation parameters to position, rotate and scale the 3D model onto the map
+    const modelTransform2 = {
+      translateX: modelAsMercatorCoordinate2.x,
+      translateY: modelAsMercatorCoordinate2.y,
+      translateZ: modelAsMercatorCoordinate2.z,
+      rotateX: modelRotate[0],
+      rotateY: modelRotate[1],
+      rotateZ: modelRotate[2],
+      /* Since the 3D model is in real world meters, a scale transform needs to be
+       * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+       */
+      scale: modelAsMercatorCoordinate2.meterInMercatorCoordinateUnits(),
+    };
+
+    //SECOND GRID HERE ///////////////////////////////////////
+    const customLayer2 = {
+      id: "3d-model2",
+      type: "custom",
+      renderingMode: "3d",
+      onAdd: function (map, gl) {
+        this.camera = new THREE.Camera();
+        this.scene = new THREE.Scene();
+
+        // create two three.js lights to illuminate the model
+        const directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        this.scene.add(directionalLight);
+
+        const directionalLight2 = new THREE.DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight2);
+        //"https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf"
+        // use the three.js GLTF loader to add the 3D model to the three.js scene
+        const loader = new GLTFLoader();
+        loader.load("/mapass/substation/scene.gltf", (gltf) => {
+          this.scene.add(gltf.scene);
+        });
+
+        this.map = map;
+
+        // use the Mapbox GL JS map canvas for three.js
+        this.renderer = new THREE.WebGLRenderer({
+          canvas: map.getCanvas(),
+          context: gl,
+          antialias: true,
+        });
+
+        this.renderer.autoClear = false;
+      },
+      render: function (gl, matrix) {
+        const rotationX = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(1, 0, 0),
+          modelTransform2.rotateX
+        );
+        const rotationY = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(0, 1, 0),
+          modelTransform2.rotateY
+        );
+        const rotationZ = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(0, 0, 1),
+          modelTransform2.rotateZ
+        );
+
+        const m = new THREE.Matrix4().fromArray(matrix);
+        const l = new THREE.Matrix4()
+          .makeTranslation(
+            modelTransform2.translateX,
+            modelTransform2.translateY,
+            modelTransform2.translateZ
+          )
+          .scale(
+            new THREE.Vector3(
+              modelTransform2.scale,
+              -modelTransform2.scale,
+              modelTransform2.scale
+            )
+          )
+          .multiply(rotationX)
+          .multiply(rotationY)
+          .multiply(rotationZ);
+
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+      },
+    };
+
+    //42.51884268158338, -92.47860570476456
+    //THIRD HERE ////////////////////////////////////
+
+    //42.53315250071414, -92.55009212574228
+    const modelOrigin3 = [-92.55009212574228, 42.53315250071414];
+    const modelAltitude3 = 35;
+
+    const modelAsMercatorCoordinate3 = mapboxgl.MercatorCoordinate.fromLngLat(
+      modelOrigin3,
+      modelAltitude3
+    );
+
+    // transformation parameters to position, rotate and scale the 3D model onto the map
+    const modelTransform3 = {
+      translateX: modelAsMercatorCoordinate3.x,
+      translateY: modelAsMercatorCoordinate3.y,
+      translateZ: modelAsMercatorCoordinate3.z,
+      rotateX: modelRotate[0],
+      rotateY: modelRotate[1],
+      rotateZ: modelRotate[2],
+      /* Since the 3D model is in real world meters, a scale transform needs to be
+       * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+       */
+      scale: modelAsMercatorCoordinate3.meterInMercatorCoordinateUnits(),
+    };
+
+    const customLayer3 = {
+      id: "3d-model3",
+      type: "custom",
+      renderingMode: "3d",
+      onAdd: function (map, gl) {
+        this.camera = new THREE.Camera();
+        this.scene = new THREE.Scene();
+
+        // create two three.js lights to illuminate the model
+        const directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        this.scene.add(directionalLight);
+
+        const directionalLight2 = new THREE.DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight2);
+        //"https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf"
+        // use the three.js GLTF loader to add the 3D model to the three.js scene
+        const loader = new GLTFLoader();
+        loader.load("/mapass/substation/scene.gltf", (gltf) => {
+          this.scene.add(gltf.scene);
+        });
+
+        this.map = map;
+
+        // use the Mapbox GL JS map canvas for three.js
+        this.renderer = new THREE.WebGLRenderer({
+          canvas: map.getCanvas(),
+          context: gl,
+          antialias: true,
+        });
+
+        this.renderer.autoClear = false;
+      },
+      render: function (gl, matrix) {
+        const rotationX = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(1, 0, 0),
+          modelTransform3.rotateX
+        );
+        const rotationY = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(0, 1, 0),
+          modelTransform3.rotateY
+        );
+        const rotationZ = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(0, 0, 1),
+          modelTransform3.rotateZ
+        );
+
+        const m = new THREE.Matrix4().fromArray(matrix);
+        const l = new THREE.Matrix4()
+          .makeTranslation(
+            modelTransform3.translateX,
+            modelTransform3.translateY,
+            modelTransform3.translateZ
+          )
+          .scale(
+            new THREE.Vector3(
+              modelTransform3.scale,
+              -modelTransform3.scale,
+              modelTransform3.scale
+            )
+          )
+          .multiply(rotationX)
+          .multiply(rotationY)
+          .multiply(rotationZ);
+
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+      },
+    };
+
+    map.on("load", () => {
+      console.log("ok");
+
       map.addLayer(customLayer, "waterway-label");
+      map.addLayer(customLayer2, "waterway-label");
+      map.addLayer(customLayer3, "waterway-label");
+    });
+
+    //    const modelOrigin = [-93.32384961062884, 41.632992292783015];
+    //    const modelOrigin2 = [-90.47484152468148, 41.54905396880391];
+    //    const modelOrigin3 = [-92.47860570476456, 42.51884268158338];
+
+    //42.028252243975224, -93.65107091233199
+    const geojson = {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            coordinates: [
+              [-93.65107091233199, 42.028252243975224],
+              [-93.65034630988335, 42.02293673512968],
+              [-93.61080806661087, 42.022764695265],
+              [-93.56982544547053, 42.00849095928635],
+              [-93.5672602242228, 41.76224805947765],
+              [-93.57584883842618, 41.7333388969314],
+              [-93.57051610258374, 41.656222138776585],
+              // //42.01660450706951, -93.61204549889963
+              // [-93.61204549889963, 42.01660450706951],
+              // //41.90510896948757, -93.6112423528411
+              // [-93.6112423528411, 41.90510896948757],
+              // //41.89573781456484, -93.60111879800014
+              // [-93.60111879800014, 41.89573781456484],
+              // //41.65651478460695, -93.56825167237233
+              // [-93.56825167237233, 41.65651478460695],
+              //41.68345702235849, -93.34719533622217
+              [-93.34719533622217, 41.68345702235849],
+              [-93.32384961062884, 41.632992292783015],
+              // [-73.97045, 40.67098],
+              // [-73.97064, 40.67078],
+              // [-73.97091, 40.67038],
+              // [-73.97107, 40.67011],
+              // [-73.97121, 40.66994],
+              // [-73.97149, 40.66969],
+              // [-73.97169, 40.66985],
+              // [-73.97175, 40.66994],
+              // [-73.97191, 40.66998],
+              // [-73.97206, 40.66998],
+              // [-73.97228, 40.67008],
+            ],
+            type: "LineString",
+          },
+        },
+      ],
+    };
+    const geojson2 = {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            coordinates: [
+              [-93.66982129637245, 42.0843008082459],
+              [-92.34358670534985, 42.09219919841053],
+              // [-93.61080806661087, 42.022764695265],
+              // [-93.56982544547053, 42.00849095928635],
+              // [-93.5672602242228, 41.76224805947765],
+              // [-93.57584883842618, 41.7333388969314],
+              // [-93.57051610258374, 441.656222138776585],
+              // // //42.01660450706951, -93.61204549889963
+              // // [-93.61204549889963, 42.01660450706951],
+              // // //41.90510896948757, -93.6112423528411
+              // // [-93.6112423528411, 41.90510896948757],
+              // // //41.89573781456484, -93.60111879800014
+              // // [-93.60111879800014, 41.89573781456484],
+              // // //41.65651478460695, -93.56825167237233
+              // // [-93.56825167237233, 41.65651478460695],
+              // //41.68345702235849, -93.34719533622217
+              // [-93.34719533622217, 41.68345702235849],
+              // [-93.32384961062884, 41.632992292783015],
+              // // [-73.97045, 40.67098],
+              // [-73.97064, 40.67078],
+              // [-73.97091, 40.67038],
+              // [-73.97107, 40.67011],
+              // [-73.97121, 40.66994],
+              // [-73.97149, 40.66969],
+              // [-73.97169, 40.66985],
+              // [-73.97175, 40.66994],
+              // [-73.97191, 40.66998],
+              // [-73.97206, 40.66998],
+              // [-73.97228, 40.67008],
+            ],
+            type: "LineString",
+          },
+        },
+      ],
+    };
+
+    map.on("load", () => {
+      map.addSource("line", {
+        type: "geojson",
+        data: geojson,
+      });
+      map.addSource("line2", {
+        type: "geojson",
+        data: geojson2,
+      });
+
+      // add a line layer without line-dasharray defined to fill the gaps in the dashed line
+      map.addLayer({
+        type: "line",
+        source: "line",
+        id: "line-background",
+        paint: {
+          "line-color": "yellow",
+          "line-width": 6,
+          "line-opacity": 0.4,
+        },
+      });
+
+      // add a line layer with line-dasharray set to the first value in dashArraySequence
+      map.addLayer({
+        type: "line",
+        source: "line",
+        id: "line-dashed",
+        paint: {
+          "line-color": "yellow",
+          "line-width": 6,
+          "line-dasharray": [0, 4, 3],
+        },
+      });
+
+      // technique based on https://jsfiddle.net/2mws8y3q/
+      // an array of valid line-dasharray values, specifying the lengths of the alternating dashes and gaps that form the dash pattern
+      const dashArraySequence = [
+        [0, 4, 3],
+        [0.5, 4, 2.5],
+        [1, 4, 2],
+        [1.5, 4, 1.5],
+        [2, 4, 1],
+        [2.5, 4, 0.5],
+        [3, 4, 0],
+        [0, 0.5, 3, 3.5],
+        [0, 1, 3, 3],
+        [0, 1.5, 3, 2.5],
+        [0, 2, 3, 2],
+        [0, 2.5, 3, 1.5],
+        [0, 3, 3, 1],
+        [0, 3.5, 3, 0.5],
+      ];
+
+      let step = 0;
+
+      function animateDashArray(timestamp) {
+        // Update line-dasharray using the next value in dashArraySequence. The
+        // divisor in the expression `timestamp / 50` controls the animation speed.
+        const newStep = parseInt((timestamp / 50) % dashArraySequence.length);
+
+        if (newStep !== step) {
+          map.setPaintProperty(
+            "line-dashed",
+            "line-dasharray",
+            dashArraySequence[step]
+          );
+          step = newStep;
+        }
+
+        // Request the next frame of the animation.
+        requestAnimationFrame(animateDashArray);
+      }
+
+      // start the animation
+      animateDashArray(0);
     });
   }, []);
 
