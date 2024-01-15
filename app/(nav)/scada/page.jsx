4ce3e1c7d80b61e2cb3c2871d12dd3bca61e32d0
Petrus Patronus <jan@Petruss-MacBook-Air.local>
2024-01-15 14:13:34 -0600

    more prog

--- app/(nav)/scada/page.jsx
+++ app/(nav)/scada/page.jsx
@@ -1,14 +1,24 @@
 "use client";
 
-import React from "react";
+import * as React from "react";
 import Map from "../../../components/scada/Map";
 import Map2 from "../../../components/scada/Map2";
+import { atom, useAtom } from "jotai";
+import { isPowerAtom } from "@/app/atom";
 
 const page = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [isPower, setIsPower] = useAtom(isPowerAtom);
+  const togglePower = () => {
+    setIsPower((prev) => !prev);
+    console.log("CLICKED");
+    console.log("STATE", isPower);
+  };
   return (
     <>
       <div className='flex flex-end'>
         <div style={{ width: "25vw", height: "95vh" }}> aaaa</div>
+        <button onClick={togglePower}>toggle power</button>
         <Map2 />
       </div>
     </>
--- app/atom.js
+++ app/atom.js
@@ -3,3 +3,9 @@ import dayjs from "dayjs";
 
 export const dateAtom = atom(dayjs("02/24/2024"));
 export const timeRangeAtom = atom(["12:00 AM", "12:00 AM"]);
+export const isPowerAtom = atom(true);
+export const mapPosAtom = atom({
+  zoom: 18,
+  pitch: 25,
+  center: { lat: 42.02842, lng: -93.65096 },
+});
